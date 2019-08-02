922668cb918c17b69f5071d477f4e4218bc8cd5d
Roman <vazhnik.roma@mail.ru>
2019-08-02 12:32:31 +0300

    Initial Commit

---
+++ HW4_2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW4_2
+//
+//  Created by Роман Важник on 02.08.2019.
+//  Copyright © 2019 Роман Важник. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
